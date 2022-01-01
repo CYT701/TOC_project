86bf4ed762574e0821d010e4f8febf9e7f62b34a
F74081200-CYT <tim31chen@gmail.com>
2022-01-02 06:22:49 +0800

    testing state2 message2

--- fsm.py
+++ fsm.py
@@ -50,7 +50,7 @@ class TocMachine(GraphMachine):
         print("I'm entering state2")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "生命樹咖啡:\nhttps://zh-tw.facebook.com/lifetreesilkroadcoffee/\n")
+        send_text_message(reply_token, "生命樹咖啡:\nhttps://www.google.com/maps?q=%E7%94%9F%E5%91%BD%E6%A8%B9%E5%92%96%E5%95%A1&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjbm_P0yZH1AhUSG6YKHZj9D0sQ_AUoAXoECAEQAw\n")
         self.go_back()
 
     def on_exit_state2(self):
