fcee0476cd732c1804cab8ae1c04bf6b0d2081a6
F74081200-CYT <tim31chen@gmail.com>
2022-01-02 06:07:11 +0800

    testing state3 message2

--- fsm.py
+++ fsm.py
@@ -12,9 +12,9 @@ class TocMachine(GraphMachine):
         #return text.lower() == "go to state1"
         return text.lower() == "沖煮技巧"
 
-    def is_going_to_state1_1_light_roast(self, event):
+    '''def is_going_to_state1_1_light_roast(self, event):
         text = event.message.text
-        return text.lower() == "淺焙"
+        return text.lower() == "淺焙"'''
 
     def is_going_to_state2(self, event):
         text = event.message.text
@@ -30,7 +30,7 @@ class TocMachine(GraphMachine):
         reply_token = event.reply_token
         #send_text_message(reply_token, "Trigger state1")
         send_text_message(reply_token, "請輸入焙度:")
-        #self.go_back()
+        self.go_back()
 
     def on_exit_state1(self):
         print("Leaving state1")
@@ -60,7 +60,7 @@ class TocMachine(GraphMachine):
         print("I'm entering state3")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "使用說明:\n輸入[沖煮技巧]或[台南咖啡推薦]以獲得更多資訊")
+        send_text_message(reply_token, "輸入[沖煮技巧]或[台南咖啡推薦]以獲得更多資訊")
         self.go_back()
     
     def on_exit_state3(self):
