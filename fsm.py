15ad0a148f944aac8ade67d8cb125e413db8eecc
F74081200-CYT <tim31chen@gmail.com>
2022-01-02 14:15:33 +0800

    add is gonib back state1

--- fsm.py
+++ fsm.py
@@ -31,6 +31,10 @@ class TocMachine(GraphMachine):
         text = event.message.text
         return text.lower() == "深焙"
 
+    def is_going_to_backstate1(self, event):
+        text = event.message.text
+        return text.lower() == "ok"
+
     def on_enter_state1(self, event):
         print("I'm entering state1")
 
@@ -66,21 +70,21 @@ class TocMachine(GraphMachine):
 
         reply_token = event.reply_token
         send_text_message(reply_token, "咖啡豆16g\n水溫:92~94度\n研磨度:二砂糖粗細\n悶蒸:30~50秒(視排氣狀況)\n手法:中小水由中心向外慢繞至150cc，待濾杯中液體流下一半後再注水至240cc")
-        self.go_back()
+        #self.go_back()
 
     def on_enter_state_mediumroast(self, event):
         print("I'm entering state_lightroast")
 
         reply_token = event.reply_token
         send_text_message(reply_token, "咖啡豆16g\n水溫:88~90度\n研磨度:二砂糖粗細\n悶蒸:20~40秒\n手法:中水由中心向外慢繞至150cc，待濾杯中液體流下一半後再中心注大水至240cc")
-        self.go_back()
+        #self.go_back()
 
     def on_enter_state_cityroast(self, event):
         print("I'm entering state_cityroast")
 
         reply_token = event.reply_token
         send_text_message(reply_token, "咖啡豆16g\n水溫:85~88度\n研磨度:二砂糖粗細略粗\n悶蒸:20秒\n手法:中水由中心注水至150cc，待濾杯中液體流下一半後再中心中水往外繞圈至240cc")
-        self.go_back()
+        #self.go_back()
 
     def on_exit_state_lightroast(self):
         print("Leaving state_lightroast")
